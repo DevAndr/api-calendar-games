8560287d078c74eda5d388c22290683a96225ba8
Илья Носенко <pitbullstar96@gmail.com>
2023-09-09 00:13:15 +0300

    feat: initial

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GamesModule } from './games/games.module';
+import { UsersModule } from './users/users.module';
+import { ListsModule } from './lists/lists.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    GamesModule,
+    UsersModule,
+    ListsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
