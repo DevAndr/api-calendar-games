1850971dea74163c5a74a1e24ddbd1f4333b9154
Илья Носенко <42933864+DevAndr@users.noreply.github.com>
2023-09-10 12:24:05 +0300

    feat: added schema for users

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,9 @@
-export class User {}
+import {Prop, Schema} from "@nestjs/mongoose";
+
+@Schema()
+export class User {
+    @Prop({required: true, unique: true, type: String})
+    email: string
+    @Prop({required: true, type: String})
+    password: string
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {InjectModel} from "@nestjs/mongoose";
+import mongoose, {Model, Schema} from "mongoose";
+import {User} from "./entities/user.entity";
 
 @Injectable()
 export class UsersService {
+  constructor( @InjectModel(User.name) private readonly userModel: Model<User>,) {
+  }
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -14,6 +19,15 @@ export class UsersService {
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
+  }
+
+  async findByEmail(email: string): Promise<User & { _id:  mongoose.Types.ObjectId }> {
+    const user = await this.userModel.find({email}).exec()
+    return  {
+      _id: user.at(0)._id,
+      email: user.at(0).email,
+      password: user.at(0).password,
+    }
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
